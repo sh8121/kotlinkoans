48683cfc27498eb608e36f2729ff02c13a658b13
부상훈 <sboo@ebaykorea.com>
2018-12-14 10:57:09 +0900

    aaa

--- Kotlin Koans/Conventions/In range/src/Task.kt
+++ Kotlin Koans/Conventions/In range/src/Task.kt
@@ -1,4 +1,6 @@
-class DateRange(val start: MyDate, val endInclusive: MyDate)/* TODO */
+class DateRange(val start: MyDate, val endInclusive: MyDate){
+    fun contains()
+}
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
     return date in DateRange(first, last)
