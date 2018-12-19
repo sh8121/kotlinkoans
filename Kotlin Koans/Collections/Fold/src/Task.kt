eac7d328c3dc01cd30cffce5ed351ef3f917c8f1
sh8121 <sh-1-no1@hanmail.net>
2018-12-20 05:44:46 +0900

    aa

--- Kotlin Koans/Collections/Fold/src/Task.kt
+++ Kotlin Koans/Collections/Fold/src/Task.kt
@@ -1,4 +1,5 @@
 // Return the set of products that were ordered by every customer
 fun Shop.getSetOfProductsOrderedByEveryCustomer(): Set<Product> {
-    TODO()
+    var list: List<Set<Product>> = customers.map { it.orders.flatMap { it.products }.toSet() }
+    return list.fold(setOf<Product>(), {accumulated, element -> accumulated.intersect(element)})
 }
--- Kotlin Koans/Collections/Partition/src/Task.kt
+++ Kotlin Koans/Collections/Partition/src/Task.kt
@@ -1,2 +1,7 @@
+
+
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = customers.filter{customer ->
+    val (delivered, undelivered) = customer.orders.partition { it.isDelivered }
+    undelivered.size > delivered.size
+}.toSet()
